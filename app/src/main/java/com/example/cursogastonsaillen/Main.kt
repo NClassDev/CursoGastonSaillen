906cd0d37598add92fd61642767aa38c2858a554
Ricardo J <nclass.dev@gmail.com>
2022-11-14 13:29:26 -0600

    DataClasss

--- app/src/main/java/com/example/cursogastonsaillen/Main.kt
+++ app/src/main/java/com/example/cursogastonsaillen/Main.kt
@@ -3,12 +3,26 @@ package com.example.cursogastonsaillen
 fun main() {
 
     val persona: Persona = Persona("Gaston", "Saillen")
-    persona.darBienvenida()
+    val persona2 = Persona()
+    persona2.nombre = "Carmen"
+    persona2.apellido = "Gonzales"
+    persona2.darBienvenida()
+
+    val dolly: Perro = Perro("Dolly", "Chihuahua")
+    val cony: Perro = Perro("Cony", "San Bernardo")
+    val milanezo: Perro = cony.copy(nombre = "Milaneso")
+    val milanezoHermano: Perro = milanezo.copy()
+
+    println("Son iguales: ${dolly == cony}")
+    println("Son iguales: ${milanezo == cony}")
+    println("Son iguales: ${milanezo == milanezoHermano}")
+
 }
 
-class Persona(val nombre: String, val apellido: String) {
+class Persona(var nombre: String ="", var apellido: String = "") {
     fun darBienvenida() {
         println("Bienvenido $nombre, $apellido")
     }
 }
 
+data class Perro(var nombre: String, var raza: String)
