f3dd8796c3cb5d3b6f7879b55ff205e617f612d5
Ricardo J <nclass.dev@gmail.com>
2022-11-14 12:58:07 -0600

    Variables

---
+++ app/src/main/java/com/example/cursogastonsaillen/Main.kt
@@ -0,0 +1,9 @@
+package com.example.cursogastonsaillen
+
+fun main() {
+
+    var a: Int = 3 // Variable mutable que puede cambiar en cualquier ciclo de la aplicacion
+    val b: Int = 4 // Variable inmutable
+    var c: Int = a + b
+    println(c)
+}
