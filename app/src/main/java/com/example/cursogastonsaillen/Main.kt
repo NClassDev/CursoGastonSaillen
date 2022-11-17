7efb47ce0d682e01e29136e60125b5a94e04b62e
Ricardo J <nclass.dev@gmail.com>
2022-11-17 15:09:47 -0600

    Delete file

--- app/src/main/java/com/example/cursogastonsaillen/Main.kt
+++
@@ -1,9 +0,0 @@
-package com.example.cursogastonsaillen
-
-fun main() {
-
-    var a: Int = 3 // Variable mutable que puede cambiar en cualquier ciclo de la aplicacion
-    val b: Int = 4 // Variable inmutable
-    var c: Int = a + b
-    println(c)
-}
