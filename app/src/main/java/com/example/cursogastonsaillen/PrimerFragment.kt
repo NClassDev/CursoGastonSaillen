c802c57bb24817598e15ee7eb54bfe0dd2d3ac90
Ricardo J <nclass.dev@gmail.com>
2022-11-18 13:18:38 -0600

    Librerias y layouts necesarios

---
+++ app/src/main/java/com/example/cursogastonsaillen/PrimerFragment.kt
@@ -0,0 +1,22 @@
+package com.example.cursogastonsaillen
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class PrimerFragment : Fragment(R.layout.fragment_primer) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
---
+++ app/src/main/java/com/example/cursogastonsaillen/SegundoFragment.kt
@@ -0,0 +1,22 @@
+package com.example.cursogastonsaillen
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SegundoFragment : Fragment(R.layout.fragment_segundo) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+}
