6c2dae584d29e3738a57de022a9798a7a2581cff
Ricardo J <nclass.dev@gmail.com>
2022-11-18 13:44:24 -0600

    Pasar Argumentos entre fragmentes con bundle

--- app/src/main/java/com/example/cursogastonsaillen/PrimerFragment.kt
+++ app/src/main/java/com/example/cursogastonsaillen/PrimerFragment.kt
@@ -5,18 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.findNavController
 
 class PrimerFragment : Fragment(R.layout.fragment_primer) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        val button = view.findViewById<Button>(R.id.btn_primer_fragment_navegar)
+        button.setOnClickListener {
+            findNavController().navigate(R.id.action_primerFragment_to_segundoFragment, bundleOf("nombre" to "Ana", "edad" to 25))
+        }
     }
 
 }
